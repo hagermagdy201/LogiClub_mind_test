bf0e8b0f6ad09b03e1222fc56447cc8ad54a507a
hagermagdy201 <hagermm2001@gmail.com>
2026-02-05 11:59:06 +0200

    Edit the Spin Wheel in Landscape Mode

--- lib/features/spin_wheel_screen/presentation/widgets/LandScape_Screen/spin_wheel_screen_body_landscape.dart
+++ lib/features/spin_wheel_screen/presentation/widgets/LandScape_Screen/spin_wheel_screen_body_landscape.dart
@@ -1,4 +1,5 @@
 import 'package:audioplayers/audioplayers.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:logiclub/core/utils/classes/assets_image.dart';
@@ -21,7 +22,25 @@ class _SpinWheelScreenBodyState extends State<SpinWheelScreenBodyLandscape> {
   String rewardValue = "";
   late AudioPlayer player = AudioPlayer();
 
-  List<String> giveaway = ["20% vouchers", "Cap", "Sunshade", "Pen"];
+  // List<String> giveaway = ["20% vouchers", "Cap", "Sunshade"];
+  Future<List<Map<String, dynamic>>> getAllGiveaways() async {
+    CollectionReference giveaways = FirebaseFirestore.instance.collection(
+      'Givaway',
+    );
+
+    QuerySnapshot snapshot = await giveaways.get();
+
+    List<Map<String, dynamic>> allGiveaways = snapshot.docs.map((doc) {
+      Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+      data['id'] = doc.id;
+      return data;
+    }).toList();
+
+    return allGiveaways;
+  }
+
+  Future<List<Map<String, dynamic>>> get giveaway async =>
+      await getAllGiveaways();
 
   Future<void> playSuccessSound() async {
     String soundPath = AssetsSound.winningsound;
@@ -68,154 +87,163 @@ class _SpinWheelScreenBodyState extends State<SpinWheelScreenBodyLandscape> {
             ],
           ),
         ),
-        // SizedBox(height: 80.h),
         Center(
           child: Padding(
-            padding: const EdgeInsets.only(top: 60.0),
-            child: GestureDetector(
-              // onPanEnd: (details) {
-              //   print(
-              //     "_______________________onPanEnd__________________________",
-              //   );
-              //   setState(() {
-              //     selected.add(Fortune.randomInt(0, giveaway.length));
-              //   });
-              // },
-              onTap: () {
-                setState(() {
-                  selected.add(Fortune.randomInt(0, giveaway.length));
-                });
-              },
-              child: SizedBox(
-                width: 0.42.sw,
-                height: 0.42.sw,
-                child: FortuneWheel(
-                  selected: selected.stream,
-                  animateFirst: false,
-                  items: [
-                    for (int i = 0; i < giveaway.length; i++)
-                      FortuneItem(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            giveaway[i],
-                            style: TextStyle(
-                              fontSize: 15.sp,
-                              fontFamily: "Chewy",
-                              fontWeight: FontWeight.bold,
+            padding: const EdgeInsets.only(top: 50.0),
+            child: FutureBuilder<List<Map<String, dynamic>>>(
+              future: getAllGiveaways(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Text('No giveaways available');
+                } else {
+                  final giveaways = snapshot.data!;
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selected.add(Fortune.randomInt(0, giveaways.length));
+                      });
+                    },
+                    child: SizedBox(
+                      width: 0.42.sw,
+                      height: 0.42.sw,
+                      child: FortuneWheel(
+                        selected: selected.stream,
+                        animateFirst: false,
+                        items: [
+                          for (int i = 0; i < giveaways.length; i++)
+                            FortuneItem(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  giveaways[i]['name']?.toString() ?? '',
+                                  style: TextStyle(
+                                    fontSize: 15.sp,
+                                    fontFamily: "Chewy",
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  softWrap: true,
+                                  overflow: TextOverflow.visible,
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              style: FortuneItemStyle(
+                                color:
+                                    Color.lerp(
+                                      color.snpinwheelPrimaryColor,
+                                      color.snpinwheelSecondaryColor,
+                                      i / (giveaways.length - 0.9),
+                                    ) ??
+                                    Color(0xFF00C9FF),
+                                borderColor: color.whiteColor,
+                                borderWidth: 4,
+                                textStyle: TextStyle(color: color.whiteColor),
+                              ),
                             ),
-                            softWrap: true,
-                            overflow: TextOverflow.visible,
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        style: FortuneItemStyle(
-                          color:
-                              Color.lerp(
-                                color.snpinwheelPrimaryColor,
-                                color.snpinwheelSecondaryColor,
-                                i / (giveaway.length - 0.9),
-                              ) ??
-                              Color(0xFF00C9FF),
-                          borderColor: color.whiteColor,
-                          borderWidth: 4,
-                          textStyle: TextStyle(color: color.whiteColor),
-                        ),
-                      ),
-                  ],
-                  onAnimationEnd: () {
-                    setState(() {
-                      rewardValue = giveaway[selected.value];
-                    });
-                    print("-----------------------$rewardValue");
-                    playSuccessSound();
+                        ],
+                        onAnimationEnd: () {
+                          setState(() {
+                            rewardValue =
+                                giveaways[selected.value]['name']?.toString() ??
+                                '';
+                          });
+                          print("-----------------------$rewardValue");
+                          playSuccessSound();
 
-                    showDialog(
-                      context: context,
-                      builder: (_) => Dialog(
-                        backgroundColor: Colors.transparent,
-                        insetPadding: EdgeInsets.all(16),
-                        child: Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            Container(
-                              width: 1.sw,
-                              height: 1.sw,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(25),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black.withOpacity(0.4),
-                                    blurRadius: 15,
-                                    offset: Offset(0, 8),
+                          showDialog(
+                            context: context,
+                            builder: (_) => Dialog(
+                              backgroundColor: Colors.transparent,
+                              insetPadding: EdgeInsets.all(16),
+                              child: Stack(
+                                alignment: Alignment.center,
+                                children: [
+                                  Container(
+                                    width: 1.sw,
+                                    height: 1.sw,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(25),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black.withOpacity(0.4),
+                                          blurRadius: 15,
+                                          offset: Offset(0, 8),
+                                        ),
+                                      ],
+                                    ),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(25),
+                                      child: Image.asset(
+                                        AssetsPaths.bluebackgroundImage,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Image.asset(
+                                        'assets/images/win.gif',
+                                        width: 0.5.sw,
+                                        height: 0.3.sw,
+                                        fit: BoxFit.cover,
+                                      ),
+                                      SizedBox(height: 20.h),
+                                      Text(
+                                        rewardValue,
+                                        style: TextStyle(
+                                          fontSize: 50,
+                                          fontFamily: "Chewy",
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      SizedBox(height: 10.h),
+                                      ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: color.primary,
+                                          padding: EdgeInsets.symmetric(
+                                            horizontal: 5,
+                                            vertical: 5,
+                                          ),
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(
+                                              16,
+                                            ),
+                                          ),
+                                          maximumSize: Size(450, 100),
+                                          minimumSize: Size(450, 100),
+                                        ),
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                          Navigator.pop(context);
+                                          Navigator.pop(context);
+                                        },
+                                        child: Text(
+                                          'Hope to see you again 👋🏻',
+                                          style: TextStyle(
+                                            fontSize: 15.sp,
+                                            fontFamily: "Chewy",
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ],
                               ),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(25),
-                                child: Image.asset(
-                                  AssetsPaths.bluebackgroundImage,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
                             ),
-                            Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Image.asset(
-                                  'assets/images/win.gif',
-                                  width: 0.5.sw,
-                                  height: 0.3.sw,
-                                  fit: BoxFit.cover,
-                                ),
-                                SizedBox(height: 20.h),
-                                Text(
-                                  rewardValue,
-                                  style: TextStyle(
-                                    fontSize: 50,
-                                    fontFamily: "Chewy",
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                SizedBox(height: 10.h),
-                                ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: color.primary,
-                                    padding: EdgeInsets.symmetric(
-                                      horizontal: 5,
-                                      vertical: 5,
-                                    ),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(16),
-                                    ),
-                                    maximumSize: Size(450, 100),
-                                    minimumSize: Size(450, 100),
-                                  ),
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                    Navigator.pop(context);
-                                    Navigator.pop(context);
-                                  },
-                                  child: Text(
-                                    'Hope to see you again 👋🏻',
-                                    style: TextStyle(
-                                      fontSize: 15.sp,
-                                      fontFamily: "Chewy",
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
+                          );
+                        },
                       ),
-                    );
-                  },
-                ),
-              ),
+                    ),
+                  );
+                }
+              },
             ),
           ),
         ),
